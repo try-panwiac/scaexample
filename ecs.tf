fd89b7293b2e11584b7339244d156387e25427b3
try-panwiac <93744932+try-panwiac@users.noreply.github.com>
2022-11-01 16:13:29 -0700

    Create ecs.tf

---
+++ ecs.tf
@@ -0,0 +1,18 @@
+resource "aws_ecs_task_definition" "service" {
+  family = "service"
+  container_definitions = jsonencode([
+    {
+      name      = "first"
+      image     = "ubuntu@sha256:dda6886d8d153a2d86f046c9335123c6151d83fd63e446b752ed8d9da261205d"
+      cpu       = 10
+      memory    = 512
+      essential = true
+      portMappings = [
+        {
+          containerPort = 80
+          hostPort      = 80
+        }
+      ]
+    }
+  ])
+}
